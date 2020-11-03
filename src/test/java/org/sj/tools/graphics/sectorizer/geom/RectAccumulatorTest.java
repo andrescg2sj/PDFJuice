b8e5a039e33a8b61999ae73c2632a46d989a7353
andrescg2sj <andres.glez.sj@gmail.com>
2020-11-03 19:38:44 +0100

    Some tests added

---
+++ src/test/java/org/sj/tools/graphics/sectorizer/geom/RectAccumulatorTest.java
@@ -0,0 +1,25 @@
+package org.sj.tools.graphics.sectorizer.geom;
+
+import java.awt.geom.Rectangle2D;
+
+import java.util.List;
+import java.util.LinkedList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RectAccumulatorTest {
+
+    @Test
+    public void testingAccumulateX() {
+	List<Rectangle2D> rects = new LinkedList<Rectangle2D>();
+	Rectangle2D a = new Rectangle2D.Double(0,0,20,10);
+	Rectangle2D b = new Rectangle2D.Double(10,0,20,20);
+
+	List<Rectangle2D> result = RectAccumulator.accumulateX(rects);
+
+	Assert.assertEquals("number of rects", 3, result.size());
+
+    }
+
+}
--- src/test/java/org/sj/tools/pdfjuice/slide/build/PosterSlideBuilderTest.java
+++ src/test/java/org/sj/tools/pdfjuice/slide/build/PosterSlideBuilderTest.java
@@ -3,6 +3,10 @@ package org.sj.tools.pdfjuice.slide.build;
 import java.util.Vector;
 import org.sj.tools.graphics.sectorizer.geom.NumberVector;
 
+import java.awt.geom.Rectangle2D;
+import java.util.Vector;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,6 +21,21 @@ public class PosterSlideBuilderTest {
 
 	int mins[] = PosterSlideBuilder.getMinIndexes(new NumberVector(a));
 	Assert.assertEquals("size", 1, mins.length);
+    }
+
+
+        @Test
+    public void testingGetVertVoids() {
+
+	List<Rectangle2D> rects = new Vector<Rectangle2D>();
+	Rectangle2D a = new Rectangle2D.Double(0,0,10, 10);
+	Rectangle2D b = new Rectangle2D.Double(30,0,10, 10);
+
+	rects.add(a);
+	rects.add(b);
+
+	NumberVector numbers = PosterSlideBuilder.getVertVoids(rects);
+	Assert.assertEquals("number of edges", 1, numbers.size());
     }
 
 }
